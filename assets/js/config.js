0bcfd02e7d94c92fe9b96555b6fcccbd9bb6970e
a84771426-eng <a84771426@gmail.com>
2026-05-19 00:25:47 -0300

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,15 @@
+/**
+ * Dados da empresa.
+ * WhatsApp: apenas números com DDI (ex: 5511999999999).
+ */
+window.SITE_CONFIG = {
+  companyName: "YURI SILVA LIMA",
+  legalName: "66.344.655 YURI SILVA LIMA",
+  whatsapp: "557781096234",
+  whatsappDisplay: "(77) 8109-6234",
+  phone: "(77) 8109-6234",
+  email: "yurilima.markion@gmail.com",
+  address: "Rua do Corcovado, 500",
+  location: "Rua do Corcovado, 500 - Vitoria da Conquista - BA, CEP 45077-676",
+  cnpj: "66.344.655/0001-95",
+};
